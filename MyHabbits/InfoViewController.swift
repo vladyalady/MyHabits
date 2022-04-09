5c3391da07195d1e1e6d218494e2d6912111f0f0
vladyaldy <vladybarinov@gmail.com>
2022-04-09 18:44:39 +0300

    Initial commot

---
+++ MyHabbits/InfoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  InfoViewController.swift
+//  MyHabbits
+//
+//  Created by Vladislav Barinov on 09.04.2022.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
